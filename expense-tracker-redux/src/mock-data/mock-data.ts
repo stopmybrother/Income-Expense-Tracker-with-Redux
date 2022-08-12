465eb4691e7a3d545417d83cb1b099fd0d32336f
Zhenya Aleksandrova <laberfo@gmail.com>
2022-08-12 23:07:34 +0300

    fix: added categories for transactions in mock-data/mock-data.ts

--- expense-tracker-redux/src/mock-data/mock-data.ts
+++ expense-tracker-redux/src/mock-data/mock-data.ts
@@ -3,5 +3,59 @@ export interface ITransaction {
     text: string;
     amount: number;
 };
+export interface ICategory {
+    id: number;
+    category: string;
+    categoryDetails: string;
+};
+
+export const CATEGORIES: ICategory[] = [
+    {
+        id: 1,
+        category: "income",
+        categoryDetails: "Salary",
+    },
+    {
+        id: 2,
+        category: "income",
+        categoryDetails: "Other earnings",
+    },
+    {
+        id: 3,
+        category: "expense",
+        categoryDetails: "Rent",
+    },
+    {
+        id: 4,
+        category: "expense",
+        categoryDetails: "Petrol",
+    },
+    {
+        id: 5,
+        category: "expense",
+        categoryDetails: "Health",
+    },
+    {
+        id: 3,
+        category: "expense",
+        categoryDetails: "Food and etc.",
+    },
+    {
+        id: 6,
+        category: "expense",
+        categoryDetails: "Shopping",
+    },
+    {
+        id: 7,
+        category: "expense",
+        categoryDetails: "Entertainment",
+    },
+    {
+        id: 8,
+        category: "expense",
+        categoryDetails: "Pernicious habits",
+    },
+];
 
 export const TRANSACTIONS: ITransaction[] = [];
+
