a46822063ef813d97ca0429b8018ed8c0f3b93c6
Zhenya Aleksandrova <laberfo@gmail.com>
2022-08-12 23:05:37 +0300

    feature: added styles for new Select-component in components/Select.ts

---
+++ expense-tracker-redux/src/styled-components/components/Select.ts
@@ -0,0 +1,99 @@
+import styled, { css } from "styled-components";
+
+interface ISelectDropdownBtn {
+    isOpened: boolean;
+    selectedCategoryColor: string;
+};
+interface ISelectItem {
+    category: string;
+};
+
+export const SelectWrapper = styled.div`
+  position: relative;
+  user-select: none;
+`;
+export const SelectContent = styled.div`
+  position: absolute;
+  top: 115%;
+  width: 100%;
+  box-shadow: 3px 3px 10px 6px rgba(0, 0, 0, 0.06);
+`;
+export const SelectItem = styled.div<ISelectItem>`
+  padding: 10px;
+  cursor: pointer;
+  background-color: ${ p => p.theme.colors.whitePinkMinLight };
+  border-bottom: 1px solid ${ p => p.theme.colors.blackSuperOpacity };
+  position: relative;
+  transition: all 0.2s linear;
+  &:hover {
+    background-color: ${ p => p.theme.colors.whitePinkLight };
+    border-bottom: 1px solid transparent;
+  }
+  &::before {
+    content: "";
+    position: absolute;
+    top: 43%;
+    right: calc(0% - 15.7px);
+    width: 41px;
+    height: 6px;
+    border-radius: 2px;
+    transform: rotate(90deg);
+    ${ p => p.category === "income" && css`
+      background-color: ${ p => p.theme.colors.greenLightOpacity };
+    ` }
+    ${ p => p.category === "expense" && css`
+      background-color: ${ p => p.theme.colors.redSuperLightOpacity };
+    ` }
+  }
+  &:last-child {
+    border-bottom: none;
+  }
+`;
+export const SelectDropdownBtn = styled.div<ISelectDropdownBtn>`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px;
+  background-color: ${ p => p.theme.colors.whiteExtraLight };
+  ${ p => p.selectedCategoryColor === "income" && css`
+    color: ${ p => p.theme.colors.darkGreen };
+  `};
+  ${ p => p.selectedCategoryColor === "expense" && css`
+    color: ${ p => p.theme.colors.darkRed };
+  `};
+  box-shadow: 3px 3px 10px 6px rgba(0, 0, 0, 0.06);
+  font-weight: 600;
+  cursor: pointer;
+  position: relative;
+  transition: all 0.2s linear;
+  &::before, ::after {
+    content: "";
+    position: absolute;
+    top: 50%;
+    width: 15px;
+    height: 2px;
+    border-radius: 3px;
+    background-color: ${ p => p.theme.colors.darkGray };
+    transition: all 0.2s linear;
+  }
+  &::before {
+    left: calc( 100% - 37px );
+    transform: rotate( 45deg );
+    ${ p => p.isOpened && css `
+      transform: rotate( -45deg );
+  ` }
+  }
+  &::after {
+    left: calc( 100% - 27px );
+    transform: rotate( -45deg );
+    ${ p => p.isOpened && css `
+      transform: rotate( 45deg );
+  ` }
+  }
+  &:hover {
+    background-color: ${ p => p.theme.colors.whiteLight };
+  }
+  &:active {
+    background-color: ${ p => p.theme.colors.white };
+  }
+`;
