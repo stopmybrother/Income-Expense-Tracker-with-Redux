b334b3161b6d4e9c01bd75c67b63760d9beb6943
Zhenya Aleksandrova <laberfo@gmail.com>
2022-08-16 22:17:18 +0300

    fix: removed counting of totalAmount in components/Balance/Balance.tsx

--- expense-tracker-redux/src/components/Balance/Balance.tsx
+++ expense-tracker-redux/src/components/Balance/Balance.tsx
@@ -1,14 +1,12 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { transactionsSelector } from "../../redux/selectors/transactionsSelector/transactionsSelector";
-import { ITransaction } from "../../mock-data/mock-data";
 import { ContainerNotCentered } from "../../styled-components/components/Container";
 import { Text } from "../../styled-components/components/Text";
 
-export const Balance = () => {
-    const transactions = useSelector( transactionsSelector );
-    const amounts = transactions.map( ( transaction: ITransaction ) => transaction.amount );
-    const total = +amounts.reduce( ( acc: number, amount: number ) => ( acc += amount ), 0 ).toFixed( 2 );
+interface IBalance {
+    totalAmount: number;
+};
+
+export const Balance = ( { totalAmount }: IBalance ) => {
     return (
         <ContainerNotCentered>
             <Text
@@ -22,7 +20,7 @@ export const Balance = () => {
                 lineHeight = { 38 }
                 fontWeight = { 700 }
             >
-                { total < 0 ? "-" : "" } ${ Math.abs( total ) }
+                { totalAmount < 0 ? "-" : "" } ${ Math.abs( totalAmount ) }
             </Text>
         </ContainerNotCentered>
     );
